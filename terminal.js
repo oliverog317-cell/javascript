f8fa3832fe01b01ce175b3afec8887c1134fa95a
Genaro Olivero <oliverog317@gmail.com>
2026-09-23 20:36:44 -0300

    primer codigo en javascript

---
+++ terminal.js
@@ -0,0 +1,71 @@
+// SUMA 
+
+let suma = 10 + 10;
+
+// Resta
+
+let resta = 10 - 1;
+
+// MULTIPLICACIÓN 
+
+let multiplicacion = 10 * 3;
+
+// MODULO O RESTO 
+
+let modulo = 21 % 4;
+
+console.log(suma)
+console.log(resta)
+console.log(multiplicacion)
+console.log(modulo)
+
+// COMPARACIONES 
+
+let igualdad = 3 == 3;
+
+console.log("Igualdad")
+console.log(igualdad)
+
+// COMPARACION CON STRING
+
+let igualdadConString = 3 == "3";
+
+console.log("Igualdad con string")
+console.log(igualdadConString)
+
+// COMPARACION ESTRICTA 
+
+let comparacionEstricta = 3 === "3";
+
+console.log("Igualdad Estricta")
+console.log(comparacionEstricta)
+
+// Mayor o igual que 
+
+let mayor = 3 >= 2;
+
+console.log("Mayor o igual que")
+console.log(mayor)
+
+// Menor o igual que
+
+let menor = 2 <= 3;
+
+console.log("Menor o igual que")
+console.log(menor)
+
+// Desigual 
+
+let desigual = 3 != "hola";
+
+console.log("Desigualdad")
+console.log(desigual)
+
+// Concatenar 
+
+let nombre = "Genaro"
+let apellido = "Olivero"
+
+let saludo = "Hola " + nombre + " " + apellido;
+
+console.log(saludo)
