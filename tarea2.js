5ff05eb89790521bef777a5380685ab9ad03822e
Genaro Olivero <oliverog317@gmail.com>
2026-09-23 21:11:19 -0300

    Tarea 2 en JavaScript

---
+++ tarea2.js
@@ -0,0 +1,43 @@
+// Basicamente, tiene que correr este archivo, e ir encontrando los errores
+// Ir manejandolo, mejorandolo, y arreglandolo
+// Lo ideal es que vayan error por error, la terminal cuando encuentra el primer error ya rompe
+
+// Plugin recomendado:
+// Error lens:
+// https://marketplace.visualstudio.com/items?itemName=usernamehw.errorlens
+
+
+const nombre = "Martin"; 
+let edad = 24; 
+const ciudad = "Buenos Aires"; 
+
+console.log("Nombre:", nombre); 
+console.log("Edad:", edad); 
+console.log("Ciudad:", ciudad); 
+
+edad = 25; 
+const profesion = "programador"; 
+console.log("Profesión:", profesion); 
+
+let salario = 850000; 
+
+console.log("Salario:", salario); 
+console.log("Tipo de nombre:", typeof nombre); 
+console.log("Tipo de edad:", typeof edad); 
+console.log("Tipo de salario:", typeof salario); 
+
+const numero1 = 100; 
+const numero2 = 25; 
+
+console.log("Suma:", numero1 + numero2); 
+console.log("Resta:", numero1 - numero2); 
+console.log("Multiplicación:", numero1 * numero2); 
+console.log("División:", numero1 / numero2); 
+
+let apellido = "Gomez"; 
+apellido = "Pérez"; 
+console.log("Apellido:", apellido); 
+
+let cantidadProductos; 
+console.log("Cantidad:", cantidadProductos); 
+console.log("Resultado:", numero1 + numero2 * 2);
